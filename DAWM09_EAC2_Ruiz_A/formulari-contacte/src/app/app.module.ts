1fbcf530ec4b7f475edd346b4438903caa2ee556
Veronica Ruiz <veronica.ruizab@gmail.com>
2024-04-08 20:25:50 +0200

    angular ready

--- DAWM09_EAC2_Ruiz_A/formulari-contacte/src/app/app.module.ts
+++ DAWM09_EAC2_Ruiz_A/formulari-contacte/src/app/app.module.ts
@@ -9,22 +9,30 @@ import { AppComponent } from './app.component';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 import { FormulariComponent } from './components/components/formulari/formulari.component';
 
+import { MatMenuModule } from '@angular/material/menu';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatCardModule } from '@angular/material/card';
+
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
 import { ReactiveFormsModule } from '@angular/forms';
+import { NavbarComponent } from './components/navbar/navbar.component';
 
 @NgModule({
-  declarations: [AppComponent, FormulariComponent],
+  declarations: [AppComponent, FormulariComponent, NavbarComponent],
   imports: [
+    MatButtonModule,
+    MatMenuModule,
+    MatToolbarModule,
+    MatCardModule,
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
     MatFormFieldModule,
     MatInputModule,
-    MatButtonModule,
     MatIconModule,
     ReactiveFormsModule,
   ],
